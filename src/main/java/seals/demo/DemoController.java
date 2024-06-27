d6ec06c40250636c45dfb39e162fba8cebf9e79d
cmseals <cmseals@tva.gov>
2024-06-27 13:12:55 -0400

    add dev tools

--- src/main/java/seals/demo/DemoController.java
+++ src/main/java/seals/demo/DemoController.java
@@ -3,12 +3,12 @@ package seals.demo;
 import java.util.HashMap;
 import java.util.Map;
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class DemoController {
-    @RequestMapping("/resource")
+    @GetMapping("/resource")
 	public Map<String,Object> home() {
 		Map<String,Object> model = new HashMap<String,Object>();
 		model.put("content", "I'm feeling really Sealy right now");
